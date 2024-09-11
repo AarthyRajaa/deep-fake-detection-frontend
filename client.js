971e89ddb5ccfbc9dd2b0dd41ec9ecc163a722e7
johntheajs <johnthomasajs@gmail.com>
2024-09-11 12:56:03 +0530

    corrected env

--- client.js
+++ client.js
@@ -1,15 +1,15 @@
 import sanityClient from '@sanity/client';
 
-const REACT_APP_PROJECT_ID = process.env.REACT_APP_PROJECT_ID;
-const REACT_APP_API_VERSION = process.env.REACT_APP_API_VERSION;
-const REACT_APP_TOKEN = process.env.REACT_APP_TOKEN;
+const VITE_APP_PROJECT_ID = import.meta.env.VITE_APP_PROJECT_ID;
+const VITE_APP_API_VERSION = import.meta.env.VITE_APP_API_VERSION;
+const VITE_APP_TOKEN = import.meta.env.VITE_APP_TOKEN;
 
 
 const client = sanityClient({
-  projectId: REACT_APP_PROJECT_ID , // Use your actual project ID
+  projectId: VITE_APP_PROJECT_ID , // Use your actual project ID
   dataset: 'production', // Use your dataset
-  apiVersion: REACT_APP_API_VERSION, // Use the latest version
-  token: REACT_APP_TOKEN, // Replace with your Sanity token if private
+  apiVersion: VITE_APP_API_VERSION, // Use the latest version
+  token: VITE_APP_TOKEN, // Replace with your Sanity token if private
   useCdn: false, // `false` if you want to ensure fresh data
 });
 
